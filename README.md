8c6a95a4c00397b60dbcff0bda2e6c962f51471e
Qexy.org <admin@qexy.org>
2016-07-02 22:05:18 +0400

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WebMCR-Reloaded-Bootstrap-3
 Bootstrap template for WebMCR Reloaded
+
+Upload folder "bootstrap_3" inside "themes" folder and change template settings in control panel
---
+++ bootstrap_3/theme.php
@@ -0,0 +1,13 @@
+<?php
+
+if(!defined("MCR")){ exit("Hacking Attempt!"); }
+
+$theme = array(
+	"ThemeName" => "Bootstrap 3.3.6",
+	"Author" => "Toster_tpl",
+	"AuthorUrl" => "http://webmcr.com",
+	"About" => "Шаблон на основе фреймворка Bootstrap 3.3.6",
+	"Version" => "1.0"
+);
+
+?>
