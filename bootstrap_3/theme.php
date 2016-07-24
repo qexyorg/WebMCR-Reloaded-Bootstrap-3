2d33614afefe8d157d4e9d9860f43d36a4dd0816
Qexy.org <admin@qexy.org>
2016-07-24 18:20:35 +0400

    For Beta 1.3.9

--- bootstrap_3/theme.php
+++ bootstrap_3/theme.php
@@ -7,7 +7,7 @@ $theme = array(
 	"Author" => "Toster_tpl",
 	"AuthorUrl" => "http://webmcr.com",
 	"About" => "Шаблон на основе фреймворка Bootstrap 3.3.6",
-	"Version" => "1.1 [WebMCR Reloaded 1.3.8]"
+	"Version" => "1.2 [WebMCR Reloaded 1.3.9]"
 );
 
 ?>
